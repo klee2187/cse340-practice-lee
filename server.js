df8ba439701f3efdb2e060be23b4d590b5273aa0
klee2187 <kkelly6@byui.edu>
2026-01-16 15:05:56 -0700

    Added content to about page

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
-// Import express using ESM syntax
+// Imports
+import { fileURLToPath } from 'url';
+import path from 'path';
 import express from 'express';
 
-// Create an instance of an Express application
+// Declare Important Variables
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Setup Express Server
 const app = express();
 
-const name = process.env.NAME;
+//Configure Express middleware
 
-// Define a route handler for the root URL ('/')
+
+//Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Declare Routes
 app.get('/', (req, res) => {
-    res.send(`Ahoy, ${name}!`)
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.send("This is my about page.")
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
 });
 
-app.use((req, res) => {
-    res.status(404).send('Page not found')
-})
-
-// Define the port number the server will listen on
-const PORT = 3000;
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+}); 
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
